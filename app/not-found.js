ab66e8cdd82686d3934806f51023781919d5eefb
chelsea <chelsea.qrt@gmail.com>
2024-04-10 17:18:47 +0800

    custome not found page (remember the location is fine)

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+    return (
+        <main className="not-found">
+            <h1>Not Found</h1>
+            <p>Unfortunately, this page does not exist.</p>
+        </main>
+    );
+}
