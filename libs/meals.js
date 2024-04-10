471a6edf7b235497fc8c8c197d2f301732d93cc3
chelsea <chelsea.qrt@gmail.com>
2024-04-10 17:14:36 +0800

    error test

--- libs/meals.js
+++ libs/meals.js
@@ -1,8 +1,12 @@
 import sql from 'better-sqlite3';
+import { resolve } from 'styled-jsx/css';
 
 const db = sql('meals.db');
 
-export function getMeals() {
+export async function getMeals() {
+    // fake wait for test loading page
+    await new Promise((resolve)=> setTimeout(resolve, 10000));
+    throw new Error('Load');
     return db
         .prepare('SELECT * FROM meals')
         .all();
